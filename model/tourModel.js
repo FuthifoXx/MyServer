ec0cba00a65985367ba1cb213b4a46d59676c311
FuthifoXx <futhifoxmaseko@gmail.com>
2025-02-19 11:59:01 +0200

    simmple server

---
+++ model/tourModel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const tourSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'], //validators
+    unique: true,
+    trim: true //trim as in strings in JS
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have difficulty']
+  },
+  ratingAverage: {
+    type: Number,
+    default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true, //trim as in strings in JS
+    required: [true, 'A tour must have a description']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image']
+  },
+  image: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;

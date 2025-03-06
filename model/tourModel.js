e8c8b17825425df67af186915517504eeadb0974
FuthifoXx <futhifoxmaseko@gmail.com>
2025-03-06 14:44:29 +0200

    105 to 107 Document,Query,Aggregation Middleware

--- model/tourModel.js
+++ model/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = mongoose.Schema(
       unique: true,
       trim: true //trim as in strings in JS
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
@@ -53,7 +55,11 @@ const tourSchema = mongoose.Schema(
       default: Date.now(),
       select: false //this is to prevent a certain field from showing
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
   {
     toJSON: { virtuals: true },
@@ -66,6 +72,54 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
+//DOCUMENT MIDDLEWARE?
+//Runs before .save() and .create() but not insertMany() command
+//aka pre save hook/middleware
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  //this keyword point to the current document
+  next();
+});
+
+// tourSchema.pre('save', function(next) {
+//   console.log('will save document...');
+//   next();
+// });
+
+// //aka post save hook/middleware
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+//QUERY MIDDLEWARE?
+// Query middleware in Mongoose allows you to run functions before (pre) or after (post) a query is executed. This is useful for modifying queries, adding filters, logging, or performing actions before/after a query runs.
+
+// tourSchema.pre('find', function(next) {
+//this regexp will find any thing that starts with find
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+  //this keyword point to the current query
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  console.log(docs);
+  next();
+});
+
+tourSchema.pre('aggregate', function(next) {
+  //this keyword point to current aggregation object
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  console.log(this.pipeline());
+  next();
+});
+
+//AGGREGATION MIDDLEWARE?
+//Aggregation middleware in Mongoose allows you to modify aggregation pipelines before execution. It is useful for adding filters, logging, or transforming data in aggregation queries.
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;

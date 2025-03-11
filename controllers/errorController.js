607b70abbdd2d59077c1be52cd1202a28be4cbad
FuthifoXx <futhifoxmaseko@gmail.com>
2025-03-11 16:54:12 +0200

    Handling Invalid Database IDs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}:${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (error, res) => {
   res.status(error.statusCode).json({
     status: error.status,
@@ -34,6 +41,8 @@ module.exports = (error, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(error, res);
   } else if (process.env.NODE_ENV === 'production') {
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+
     sendErrorProd(error, res);
   }
 };

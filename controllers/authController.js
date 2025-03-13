643add081d221106073e786eee6fe59660f9f396
FuthifoXx <futhifoxmaseko@gmail.com>
2025-03-13 16:02:34 +0200

    129. Signing up Users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,23 @@
+const jwt = require('jsonwebtoken');
 const User = require('../model/userModal');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
+  //   const newUser = await User.create({
+  //     name: req.body.name,
+  //     email: req.body.email,
+  //     password: req.boy.password,
+  //     passwordConfirm: req.body.passwordConfirm
+  //   });
+  // creating the PAYLOAD and SECRET the token will be created automatically
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser
     }

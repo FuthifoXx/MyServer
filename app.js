83624a150f183d0089fc74e88272fe2a30f4733e
FuthifoXx <futhifoxmaseko@gmail.com>
2025-03-21 13:40:23 +0200

    144 Settin Security HTTP Headers with Helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,10 +11,16 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1)GLOBAL MIDDLEWARES
+
+//Set security HTTP headers
+app.use(helmet()); //It alway best to put it as first of our middlewares
+
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+//Limiting requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -21,13 +28,16 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+//Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+//Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.headers);
-
+  // console.log(req.headers);
   next();
 });
 
